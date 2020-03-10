d3d828ad3c2ae29815c71735d25eb1db005b2245
duai <duaifzn@gmail.com>
2020-03-10 12:23:15 +0800

    models test complete

--- models/user.js
+++ models/user.js
@@ -1,8 +1,27 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING,
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
+    User.hasMany(models.Reply)
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Like)
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'followings'
+    }),
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'followers',
+      })
   };
   return User;
 };
