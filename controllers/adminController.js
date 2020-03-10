2d1c063f5c096d089c7f5ca0d8ea075f7f0e4c5a
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-03-11 02:22:46 +0800

    Add admin users and main page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,9 @@ const db = require('../models')
 const User = db.User
 
 const adminController = {
-
+  getUsers: (req, res) => {
+    res.render('admin/users')
+  }
 }
 
 module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
+const adminController = require('../controllers/adminController.js')
+
 module.exports = app => {
+  // /main
   app.get('/', (req, res) => { res.send('hello word!') })
+  // admin
+  app.get('/admin', (req, res) => res.redirect('/admin/users'))
+  app.get('/admin/users', adminController.getUsers)
 }
