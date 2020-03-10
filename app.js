10cfbaeed90f5463e43cb776453403eb0365c2b6
duai <duaifzn@gmail.com>
2020-03-10 15:47:10 +0800

    add controller views routes

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const helpers = require('./_helpers');
 
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set("view engine", "handlebars")
+
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+require('./routes')(app)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
