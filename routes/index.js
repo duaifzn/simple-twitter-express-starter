22aefba41ab48d1c9311527bfc2c57a1beb3c8dd
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-03-11 11:53:42 +0800

    Add navbar to main layout

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const adminController = require('../controllers/adminController.js')
 
 module.exports = app => {
   // /main
-  app.get('/', (req, res) => { res.send('hello word!') })
+  app.get('/', (req, res) => { res.redirect('/tweets') })
   // admin
   app.get('/admin', (req, res) => res.redirect('/admin/users'))
   app.get('/admin/users', adminController.getUsers)
+  // tweets
+  app.get('/tweets', (req, res) => { res.render('tweets') })
 }
