9cbb5b4632b76b5a974dbbc7789898413b71b157
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-03-12 16:46:04 +0800

    Add fake data of tweets

--- models/tweet.js
+++ models/tweet.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
     UserId: DataTypes.INTEGER,
-    description: DataTypes.TEXT,
-  }, {});
+    description: DataTypes.TEXT
+  }, {})
   Tweet.associate = function (models) {
     Tweet.hasMany(models.Reply)
     Tweet.hasMany(models.Like)
     Tweet.belongsTo(models.User)
-  };
-  return Tweet;
-};
+  }
+  return Tweet
+}
