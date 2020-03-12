7367714712bd7c37cd5e47e6337cafb4c039deea
duai <duaifzn@gmail.com>
2020-03-12 13:48:50 +0800

    登入註冊功能完成

--- controllers/userController.js
+++ controllers/userController.js
@@ -87,7 +87,7 @@ const userController = {
       })
   },
   logOut: (req, res) => {
-    req.flash('success_messages', 'Logout successfully')
+    req.flash('success_messages', '登出成功')
     req.logout()
     res.redirect('/signin')
   },
