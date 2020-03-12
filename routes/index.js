d1bd706779f94a1536ec57ecdca7edb63d21e906
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-03-12 12:28:35 +0800

    Add unAuthenticated status, and tag all route proper status

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const userController = require('../controllers/userController')
 const tweetController = require('../controllers/tweetController')
 
 module.exports = (app, passport) => {
+  const unAuthenticated = (req, res, next) => { // 限制登入頁只能在未登入時看到用
+    if (!req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/tweets')
+  }
+
   const authenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
       return next()
@@ -18,18 +25,18 @@ module.exports = (app, passport) => {
   }
 
   // 轉向/tweets
-  app.get('/', (req, res) => {
+  app.get('/', authenticated, (req, res) => {
     res.redirect('/tweets')
   })
 
   // 登入頁面
-  app.get('/signin', userController.signInPage)
-  //登入
-  app.post('/signin', passport.authenticate('local', {
+  app.get('/signin', unAuthenticated, userController.signInPage)
+  // 登入
+  app.post('/signin', unAuthenticated, passport.authenticate('local', {
     failureRedirect: '/signin',
     failureFlash: true
   }), userController.signIn)
-  //註冊頁面
+  // 註冊頁面
   app.get('/signup', userController.signUpPage)
   // 註冊
   app.post('/signup', userController.signUp)
@@ -37,42 +44,44 @@ module.exports = (app, passport) => {
   app.get('/logout', userController.logOut)
 
   // 看見某一使用者的推播牆，以及該使用者簡介
-  app.get('/users/:id/tweets', userController.tweetPage)
+  app.get('/users/:id/tweets', authenticated, userController.tweetPage)
   // 編輯自己的介紹 (表單頁)
-  app.get('/users/:id/edit', userController.editUserPage)
+  app.get('/users/:id/edit', authenticated, userController.editUserPage)
   // 編輯自己的介紹 (寫入資料庫)
-  app.post('/users/:id/edit', userController.editUser)
+  app.post('/users/:id/edit', authenticated, userController.editUser)
   // 看見某一使用者正在關注的使用者
-  app.get('/users/:id/followings', userController.followingPage)
+  app.get('/users/:id/followings', authenticated, userController.followingPage)
   // 看見某一使用者的跟隨者
-  app.get('/users/:id/followers', userController.followerPage)
+  app.get('/users/:id/followers', authenticated, userController.followerPage)
   // 看見某一使用者按過 like 的推播
-  app.get('/users/:id/likes', userController.likePage)
+  app.get('/users/:id/likes', authenticated, userController.likePage)
 
   // 看見站內所有的推播，以及跟隨者最多的使用者 (設為前台首頁)
-  app.get('/tweets', tweetController.tweetHomePage)
+  app.get('/tweets', authenticated, tweetController.tweetHomePage)
   // 將新增的推播寫入資料庫
-  app.post('/tweets', tweetController.createTweet)
+  app.post('/tweets', authenticated, tweetController.createTweet)
   // 可以在這頁回覆特定的 tweet，並看見 tweet 主人的簡介
-  app.get('/tweets/:tweet_id/replies', tweetController.tweetReplyPage)
+  app.get('/tweets/:tweet_id/replies', authenticated, tweetController.tweetReplyPage)
   // 將回覆的內容寫入資料庫
-  app.post('/tweets/:tweet_id/replies', tweetController.createTweetReply)
+  app.post('/tweets/:tweet_id/replies', authenticated, tweetController.createTweetReply)
 
   // 新增一筆 followship 記錄
-  app.post('/followships', userController.createFollowship)
+  app.post('/followships', authenticated, userController.createFollowship)
   // 刪除一筆 followship 記錄
-  app.delete('/followships/:followingId', userController.deleteFollowship)
+  app.delete('/followships/:followingId', authenticated, userController.deleteFollowship)
 
   // 新增一筆 like 記錄
-  app.post('/tweets/:id/like', tweetController.createLike)
+  app.post('/tweets/:id/like', authenticated, tweetController.createLike)
   // 刪除一筆 like 記錄
-  app.post('/tweets/:id/unlike', tweetController.deleteLike)
+  app.post('/tweets/:id/unlike', authenticated, tweetController.deleteLike)
 
-  app.get('/admin', (req, res) => res.redirect('/admin/tweets'))
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/tweets'))
   // 看見站內所有的推播 (設為後台首頁)
-  app.get('/admin/tweets', adminController.adminHomePage)
+  app.get('/admin/tweets', authenticatedAdmin, adminController.adminHomePage)
   // 刪除其他使用者的推文
-  app.delete('/admin/tweets/:id', adminController.deleteTweet)
+  app.delete('/admin/tweets/:id', authenticatedAdmin, adminController.deleteTweet)
   // 看見站內所有的使用者
-  app.get('/admin/users', adminController.adminUserPage)
+  app.get('/admin/users', authenticatedAdmin, adminController.adminUserPage)
+
+  app.all('*', tweetController.redirectInvalidUrl) // 避免404當掉
 }
