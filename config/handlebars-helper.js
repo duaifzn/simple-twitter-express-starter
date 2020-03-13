24a475f8dbfb643217280faf426440222e0398f6
duai <duaifzn@gmail.com>
2020-03-13 20:12:38 +0800

    add follower,following page

--- config/handlebars-helper.js
+++ config/handlebars-helper.js
@@ -11,10 +11,31 @@ module.exports = {
   moment: function (a) {
     return moment(a).fromNow()
   },
-  countReplies: function (tweet) {
+  tweetCountReplies: function (tweet) {
     return tweet.Replies.length
   },
-  countLikes: function (tweet) {
+  tweetCountLikes: function (tweet) {
     return tweet.Likes.length
   },
+  userCountTweets: function (user) {
+    return user.Tweets.length
+  },
+  userCountLikes: function (user) {
+    return user.Likes.length
+  },
+  userCountFollowings: function (user) {
+    return user.Followings.length
+  },
+  userCountFollowers: function (user) {
+    return user.Followers.length
+  },
+  ifFollowed: function (a, b, options) {
+    if (a === b) {
+      return options.fn(this)
+    }
+    else {
+      return options.inverse(this)
+    }
+  },
+
 }
