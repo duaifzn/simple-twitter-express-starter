dc623b8ddb197e7281028b1ff802dee67b7566da
duai <duaifzn@gmail.com>
2020-03-13 15:58:07 +0800

    add tweetReplyPage

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -47,10 +47,36 @@ const tweetController = {
 
   },
   tweetReplyPage: (req, res) => {
+    Tweet.findByPk(req.params.tweet_id, {
+      include: [
+        {
+          model: User, include: [
+            Tweet,
+            { model: User, as: 'Followings' },
+            { model: User, as: 'Followers' }]
+        },
+        Reply,
+        Like
+      ]
+    }).then(tweet => {
 
+      let likeNumber = tweet.Likes.length
+      let followerNumber = tweet.User.Followings.length
+      let followingNumber = tweet.User.Followers.length
+      let tweetNumber = tweet.User.Tweets.length
+      let replyNumber = tweet.Replies.length
+      let isFollowed = req.user.Followings.map(d => d.id).includes(tweet.UserId)
+      return res.render('tweetReplyPage', JSON.parse(JSON.stringify({ tweet: tweet, likeNumber: likeNumber, followerNumber: followerNumber, followingNumber, followingNumber, tweetNumber: tweetNumber, replyNumber: replyNumber, isFollowed: isFollowed })))
+    })
   },
   createTweetReply: (req, res) => {
-
+    Reply.create({
+      UserId: req.body.userId,
+      TweetId: req.params.tweet_id,
+      comment: req.body.text
+    }).then(() => {
+      return res.redirect('back')
+    })
   },
   createLike: (req, res) => {
 
