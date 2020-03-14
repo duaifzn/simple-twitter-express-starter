f34c3e3ab2a0cf41b47aca0052e0ccc6c48e408a
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-03-14 18:05:48 +0800

    Fix the logic of twitter preview

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,7 +6,7 @@ const adminController = {
   adminHomePage: (req, res) => {
     return Tweet.findAll().then(tweets => {
       for (let i = 0; i < tweets.length; i++) {
-        tweets[i].description = tweets[i].description !== null ? (tweets[i].description.slice(0, 50) + '...') : '' // 只擷取前50字元顯示
+        tweets[i].description = tweets[i].description.length > 50 ? (tweets[i].description.slice(0, 50) + '...') : tweets[i].description // 只擷取前50字元顯示，此外顯示原文
       }
       return res.render('admin/tweets', JSON.parse(JSON.stringify({ tweets })))
     })
