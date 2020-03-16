e5f7efa53ee9085fa21610c34a06458e24d260ff
duai <duaifzn@gmail.com>
2020-03-16 17:19:19 +0800

    set websocket

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
+const expressWs = require('express-ws')(app);
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: require('./config/handlebars-helper') }))
 app.set('view engine', 'handlebars')
@@ -26,11 +27,25 @@ app.use((req, res, next) => {
   next()
 })
 
+
+
+// app.ws('/', function (ws, req) {
+//   console.log('socket connected');
+//   ws.on('message', function (msg) {
+//     console.log("aaaa")
+//     console.log(msg);
+//     ws.send("work")
+//   });
+
+// }).get('/tweets', (req, res) => {
+//   res.send('get /tweets !!')
+// })
+
+require('./routes')(app, passport) // 教材U16表示需要放在 app.js 的最後一行，因為按照由上而下的順序，當主程式把 app (也就是 express() ) 傳入路由時，程式中間做的樣板引擎設定、伺服器設定，也要一併透過 app 變數傳進去？
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-require('./routes')(app, passport) // 教材U16表示需要放在 app.js 的最後一行，因為按照由上而下的順序，當主程式把 app (也就是 express() ) 傳入路由時，程式中間做的樣板引擎設定、伺服器設定，也要一併透過 app 變數傳進去？
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
