4e074bb6701e9f79deab496ee29fdeba965d5d61
duai <duaifzn@gmail.com>
2020-03-30 15:34:18 +0800

    debug

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,7 @@ const userController = {
         { model: User, as: 'Followings' }
       ]
     }).then(user => {
-
+      console.log(user)
       const isFollowed = user.Followers.map(u => u.id).includes(helpers.getUser(req).id)
       Tweet.findAll({ include: [Like, Reply, User], where: { UserId: req.params.id } })
         .then(tweets => {
